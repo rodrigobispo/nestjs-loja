4873ca41af6fd0824f5fa9bfa0098ec6a54bf0f5
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-17 06:48:49 -0300

    Criação e lista de usuários; início.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsuarioController } from './usuario.controller';
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [UsuarioController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/usuario.repository.ts
@@ -0,0 +1,12 @@
+
+export class UsuarioRepository {
+  private usuarios = [];
+
+  async salvar(usuario) {
+    this.usuarios.push(usuario);
+  }
+
+  async listar() {
+    return this.usuarios;
+  }
+}
