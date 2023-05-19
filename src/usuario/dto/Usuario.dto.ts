c50334402f53ab157667a00dcc2f91f27429583c
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-19 06:33:05 -0300

    Validação assíncrona checando e-mail já existente de usuário.

--- src/usuario/dto/Usuario.dto.ts
+++ src/usuario/dto/Usuario.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
+import { EmailEhUnico } from "../validacao/email-eh-unico.validator"
 
 export class UsuarioDTO {
 
@@ -6,6 +7,7 @@ export class UsuarioDTO {
   nome: string
 
   @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
+  @EmailEhUnico({ message: 'Já existe um usuário com este e-mail.' })
   email: string
 
   @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -12,4 +12,11 @@ export class UsuarioRepository {
   async listar() {
     return this.usuarios;
   }
+
+  async jaExisteComEmail(email: string) {
+    const possivelUsuarioComEmail = this.usuarios.find(
+      usuario => usuario.email === email
+    );
+    return possivelUsuarioComEmail !== undefined;
+  }
 }
