05e99eab7ae028a3763c33e4fa8e59ca35be40d4
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-05-24 20:30:43 -0300

    Ajuste de mapeamento, ids.

--- src/entity/ProdutoCaracteristica.entity.ts
+++ src/entity/ProdutoCaracteristica.entity.ts
@@ -1,11 +1,15 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("produto_caracteristicas")
 export class ProdutoCaracteristicaEntity {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
   
   @Column({ name: "nome", length: 100, nullable: false })
   nome: string;
 
   @Column({ name: "descricao", length: 100, nullable: false })
   descricao: string;
+
 }
--- src/entity/ProdutoImagem.entity.ts
+++ src/entity/ProdutoImagem.entity.ts
@@ -1,14 +1,19 @@
 import {
   Entity,
-  Column
+  Column,
+  PrimaryGeneratedColumn
 } from 'typeorm';
 
 @Entity({ name: "produto_imagens" })
 export class ProdutoImagemEntity {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
   
   @Column({ name: "url", length: 100, nullable: false})
   url: string;
 
   @Column({ name: "descricao", length: 100, nullable: false})
   descricao: string;
+
 }
